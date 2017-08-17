a29bacbdec8fe56cc07c36c298a1ab2674b640b3
wing <zlcnkkm@21cn.com>
2017-08-17 23:43:40 +0800

    Initial Commit

---
+++ Art G Pro/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Art G Pro
+//
+//  Created by wing on 2017/8/17.
+//  Copyright © 2017年 Magic Install. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Art G Pro/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Art G Pro
+//
+//  Created by wing on 2017/8/17.
+//  Copyright © 2017年 Magic Install. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
